4648fef958372d8d57ffc6d32184299c0829b7e5
Fabs <fsalaman@gmail.com>
2018-12-12 00:25:11 -0600

    Initial Release

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
 
-namespace profmerge
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace profmerge{
+    public class Bio {
+        public Person person { get; set; }
+    }
+    public class Person{
+        public String id { get; set; }
+        public String name { get; set; }
+        public String professionalHeadline { get; set; }
+    }
+    
+    class Principal{
+        public static void Main(String[] args){
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://torre.bio");
+            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+            var request = new RestRequest();
+            request.Resource = "api/bios/rhfsalaman";
+
+            IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+            Bio bio = JsonConvert.DeserializeObject<Bio>(response.Content);
+            Console.WriteLine(bio.person.id);
+            Console.WriteLine(bio.person.name);
+            Console.WriteLine(bio.person.professionalHeadline);
         }
     }
 }
